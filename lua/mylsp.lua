5e1d909730d38c4ee277d7700f37b86776eb1573
paperbenni <paperbenni@gmail.com>
2023-02-15 12:07:02 +0100

    replace sumneko_lua

--- lua/mylsp.lua
+++ lua/mylsp.lua
@@ -98,7 +98,7 @@ then
         capabilities = capabilities,
         on_attach = on_attach
     }
-    lspconfig.sumneko_lua.setup {
+    lspconfig.lua_ls.setup {
         capabilities = capabilities,
         on_attach = on_attach,
         settings = {
