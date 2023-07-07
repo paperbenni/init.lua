656437bf18b11e3de722277ba64f36a56f36cb93
paperbenni <paperbenni@gmail.com>
2023-07-07 12:05:09 +0200

    switch to lazy.nvim from packer

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,16 +1,16 @@
--- ensure the packer plugin manager is installed
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-        vim.cmd([[packadd packer.nvim]])
-        return true
-    end
-    return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
 local potato = require("mypotato")
 
 if potato then
@@ -19,134 +19,133 @@ else
     My_completion_engine = 'mycmp'
 end
 
-require('packer').startup(function(use)
-    use 'lewis6991/impatient.nvim'
-    use 'wbthomason/packer.nvim'
+require("lazy").setup({
 
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-eunuch'
-    use 'tpope/vim-surround'
-    use 'tpope/vim-repeat'
+    'lewis6991/impatient.nvim',
 
-    use {
-        "folke/zen-mode.nvim",
-        config = function()
-            require("zen-mode").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    }
+    'tpope/vim-commentary',
+    'tpope/vim-fugitive',
+    'tpope/vim-eunuch',
+    'tpope/vim-surround',
+    'tpope/vim-repeat',
 
-    use "lukas-reineke/indent-blankline.nvim"
-    use 'mattn/emmet-vim'
-    use 'jose-elias-alvarez/null-ls.nvim'
+    { "folke/zen-mode.nvim", opts = {} },
 
+    "lukas-reineke/indent-blankline.nvim",
+    'mattn/emmet-vim',
+    'jose-elias-alvarez/null-ls.nvim',
 
-    use 'tpope/vim-dadbod'
-    use 'kristijanhusak/vim-dadbod-ui'
+    -- TODO: more lazy loading
+    'tpope/vim-dadbod',
+    'kristijanhusak/vim-dadbod-ui',
 
-    use { 'vimwiki/vimwiki', branch = 'dev' }
-    use 'paperbenni/Calendar.vim'
-    use 'michal-h21/vim-zettel'
+    { 'vimwiki/vimwiki',     branch = 'dev' },
 
-    use 'lervag/vimtex'
+    'paperbenni/Calendar.vim',
+    'michal-h21/vim-zettel',
 
-    use 'mhinz/vim-startify'
-    use 'folke/tokyonight.nvim'
-    use 'joshdick/onedark.vim'
+    'lervag/vimtex',
 
-    if not potato
-    then
-        use 'xiyaowong/nvim-transparent'
-        use {
-            'j-hui/fidget.nvim',
-            tag = 'legacy'
+    'mhinz/vim-startify',
+
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+
+    'nvim-treesitter/nvim-treesitter',
+
+    { "catppuccin/nvim",   name = "catppuccin" },
+
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        -- or                              , branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            {
+                'nvim-telescope/telescope-fzf-native.nvim',
+                build =
+                'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+            },
+            { 'jvgrootveld/telescope-zoxide', enabled = not potato }
         }
-        use "norcalli/nvim-colorizer.lua"
-        use { "folke/which-key.nvim" }
-        use 'psliwka/vim-smoothie'
-        use 'machakann/vim-highlightedyank'
-        use 'lewis6991/gitsigns.nvim'
-        use 'ray-x/lsp_signature.nvim'
-        use 'github/copilot.vim'
-        use 'jvgrootveld/telescope-zoxide'
-        use "m4xshen/hardtime.nvim"
+    },
 
-        use {
-            "SmiteshP/nvim-navbuddy",
-            requires = {
-                "neovim/nvim-lspconfig",
-                "SmiteshP/nvim-navic",
-                "MunifTanjim/nui.nvim"
-            }
+    { 'j-hui/fidget.nvim', tag = 'legacy',     enabled = not potato, },
+    {
+        "folke/which-key.nvim",
+        enabled = not potato,
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
         }
+    },
 
-        -- debugging
-        use 'mfussenegger/nvim-dap'
-        use 'rcarriga/nvim-dap-ui'
-        use 'simrat39/rust-tools.nvim'
-        use 'mfussenegger/nvim-dap-python'
-    end
-
-    use { "catppuccin/nvim", as = "catppuccin" }
-
-    use {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "neovim/nvim-lspconfig",
-    }
-    use 'nvim-treesitter/nvim-treesitter'
-    -- use 'nvim-treesitter/nvim-treesitter-context'
-
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use { 'nvim-telescope/telescope-fzf-native.nvim',
-        run =
-        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-
-
-    if My_completion_engine == 'mycmp'
-    then
-        use 'hrsh7th/cmp-nvim-lsp'
-        use 'hrsh7th/cmp-buffer'
-        use 'hrsh7th/cmp-path'
-        use 'hrsh7th/nvim-cmp'
-        use 'ray-x/cmp-treesitter'
-
-        use 'rafamadriz/friendly-snippets'
-        use({ "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" })
-        use 'saadparwaiz1/cmp_luasnip'
-    elseif My_completion_engine == 'mycoq' then
-        use { 'ms-jpq/coq_nvim', branch = 'coq' }
-        use { 'ms-jpq/coq.artifacts', branch = 'artifacts' }
-        use { 'ms-jpq/coq.thirdparty', branch = '3p' }
-    end
-
-
-
-    use {
+    {
         'nvim-tree/nvim-tree.lua',
-        requires = {
+        dependencies = {
             'nvim-tree/nvim-web-devicons', -- optional, for file icons
         },
         tag = 'nightly'                    -- optional, updated every week. (see issue #1193)
-    }
-    use 'akinsho/bufferline.nvim'
-    use 'hoob3rt/lualine.nvim'
+    },
 
-    use "windwp/nvim-autopairs"
-end)
-
+    'akinsho/bufferline.nvim',
+    'hoob3rt/lualine.nvim',
+    "windwp/nvim-autopairs",
 
 
--- the first run will install packer and our plugins
-if packer_bootstrap then
-    require("packer").sync()
-    return
-end
+    {
+        'hrsh7th/nvim-cmp',
+        enabled = not potato,
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'ray-x/cmp-treesitter',
+            'rafamadriz/friendly-snippets',
+            { "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" },
+            'saadparwaiz1/cmp_luasnip'
+        }
+    },
+    {
+        'ms-jpq/coq_nvim',
+        enabled = potato,
+        dependencies = {
+            { 'ms-jpq/coq.artifacts',  branch = 'artifacts' },
+            { 'ms-jpq/coq.thirdparty', branch = '3p' },
+        }
+    },
+
+    {
+        "SmiteshP/nvim-navbuddy",
+        enabled = not potato,
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "SmiteshP/nvim-navic",
+            "MunifTanjim/nui.nvim"
+        }
+    },
+
+    { 'github/copilot.vim',            enabled = not potato },
+
+    { 'xiyaowong/nvim-transparent' },
+    { "norcalli/nvim-colorizer.lua",   enabled = not potato },
+    { 'psliwka/vim-smoothie',          enabled = not potato },
+    { 'machakann/vim-highlightedyank', enabled = not potato },
+    { 'lewis6991/gitsigns.nvim',       enabled = not potato },
+    { 'ray-x/lsp_signature.nvim',      enabled = not potato },
+
+    { 'mfussenegger/nvim-dap',         enabled = not potato },
+    { 'rcarriga/nvim-dap-ui',          enabled = not potato },
+    { 'simrat39/rust-tools.nvim',      enabled = not potato },
+    { 'mfussenegger/nvim-dap-python',  enabled = not potato },
+
+
+})
+
