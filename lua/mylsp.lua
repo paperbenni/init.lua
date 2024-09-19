489ce3e5e33600b966c685b11e78717e1e91b6a4
paperbenni <paperbenni@gmail.com>
2024-09-19 16:18:06 +0200

    add descriptions to keymaps

--- lua/mylsp.lua
+++ lua/mylsp.lua
@@ -25,9 +25,14 @@ local on_attach = function(client, bufnr)
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+
+    local function setkey(mode, key, func, description)
+        vim.keymap.set(mode, key, func, { noremap = true, silent = true, buffer = bufnr, desc = description })
+    end
+
+    setkey('n', 'gD', vim.lsp.buf.declaration, "Go to declaration")
+    setkey('n', 'gd', vim.lsp.buf.definition, "Go to definition")
+    setkey('n', 'K', vim.lsp.buf.hover, "Show hover")
     -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
     -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
     -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
@@ -36,11 +41,12 @@ local on_attach = function(client, bufnr)
     --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     -- end, bufopts)
     -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<leader><F2>', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>x', vim.lsp.buf.code_action, bufopts)
+
+    setkey('n', '<leader><F2>', vim.lsp.buf.rename, "Rename")
+    setkey('n', '<leader>x', vim.lsp.buf.code_action, "Code Action")
     -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<space>F', function() vim.lsp.buf.format { async = true } end, bufopts)
-    vim.keymap.set('n', '<space>N', require("nvim-navbuddy").open, bufopts)
+    setkey('n', '<leader>F', function() vim.lsp.buf.format { async = true } end, 'LSP Format buffer')
+    setkey('n', '<leader>N', require("nvim-navbuddy").open, 'Open NavBuddy')
 end
 local lspconfig = require 'lspconfig'
 
